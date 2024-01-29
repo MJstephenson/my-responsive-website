eba12479f1f3b6d9dd50671275210b58144f07a4
Mark Stephenson <cyclomark@gmail.com>
2024-01-29 10:16:50 +0000

    Add toggle functionality to hamburger menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// code for the toggle hamburger menu for mobile screen
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    if (navLinks) {
+        navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+    }
+}
