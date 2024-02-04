c84a6e5efe5338504fadef079bf0057eb7d8776b
Mark Stephenson <cyclomark@gmail.com>
2024-02-04 11:06:48 +0000

    Add instagram section

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ function toggleMenu() {
         navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
     }
 }
+
+  // Create a new Date object for the current date and time
+  const instadate = new Date();
+  
+  // Array of month names
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+  // Gets the current month as text
+  const monthText = months[instadate.getMonth()];
+  
+  // Gets the current date in numbers
+  const dateNumber = instadate.getDate();
+  
+  // Gets the current year
+  const year = instadate.getFullYear();
+  
+  // Combines the month, date, and year in this format
+  const dateString = `${monthText} ${dateNumber}, ${year}`;
+  
+  // Inserts the date string into the <span> tag with id="date"
+  document.getElementById("date").innerText = dateString;
